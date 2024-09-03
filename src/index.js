47bca6a66e86c9b9928a29f53679ee59d102b51d
JeanGargano <159099449+JeanGargano@users.noreply.github.com>
2024-09-03 01:00:21 -0500

    Trabajando en el login

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Stylesheet/Home/index.css';
 import App from './Components/Home/App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> 
+      <Routes>
+        <Route path="/" element={<App />} /> {/* Página principal */}
+        <Route path="/login" element={<Login />} /> {/* Página de Login */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
